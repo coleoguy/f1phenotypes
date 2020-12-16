3639ad3aef37e55d5d54b24d09450a06afcdacc0
JamieAlfieri <j.alfieri@tamu.edu>
2020-12-16 10:02:35 -0600

    added no epistasis as epitype

--- simulationcode-parallel-hb.R
+++ simulationcode-parallel-hb.R
@@ -18,7 +18,7 @@ afreq <- list(c(0.5,0.5), c(0.6, 0.4), c(0.7, 0.3), c(0.8, 0.2), c(0.9,0.1), c(1
 hset <- c("all_add", "all_dom", "half_dom")
 esize <- list(c(0.91, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01),
               rep(0.1, 10))
-epi.type <- c("addbyadd", "addbydom", "dombyadd", "dombydom")
+epi.type <- c("noepi", "addbyadd", "addbydom", "dombyadd", "dombydom")
 epipair <- c(0,1,2,5)
 
 #Here i am setting up my output structure
@@ -44,7 +44,7 @@ x <- foreach(i=1:length(afreq), .combine="c") %dopar% { #this loops through the 
           counter <- counter +1
         }
         if(epipair[m]>0){ #if epipair is greater than 0, then record the output
-          for(n in 1:length(epi.type)){ #this loops through different epi.types
+          for(n in 2:length(epi.type)){ #this loops through different epi.types
             output[[counter]] <- simulate(N = N, loci = loci, effect.size = esize[[k]],
                                           afreq = afreq[[i]], gsize = gsize,
                                           iter = iter, s.size = s.size, epipair = epipair[m],
